220b1e09e3cc9817bb8d286ee15ad768624ffdec
sam47-asfaw <sami.amare47@gmail.com>
2023-12-17 12:45:36 -0500

    added predict script

--- predict.py
+++ predict.py
@@ -8,21 +8,38 @@ with open('./model_C=1.0.bin', 'rb') as f_in:
 
 app = Flask('churn')
 
+#transform the 
+def transform(customer):
+    return dv.transform(customer)
+
+
+
 @app.route('/predict', methods= ['POST'])
 def predict():
     customer = request.get_json()
-    X = dv.transform(customer)
-    prediction = float(model.predict_proba(X)[0,1])
-    churn = bool(prediction >= 0.5)
+    X = transform(customer)
+    prediction = float(model.predict_proba(X))
+    churn = bool(prediction < 0.5)
 
+    
     result = {
         'Churn Probablility': prediction,
-        'churn': churn
+        'churn': churn,
+        'Customer Status': ''
     }
 
-    return jsonify(result)
+    if churn < 0.5:
+        result['Customer Status'] = 'Customer is likely to churn, send promotional email.'
+        return jsonify(result)
+    elif churn >= 0.5 and churn < 1.5:
+        result['Customer Status'] = 'Welcome to our telecom service.'
+        return jsonify(result)
+    else:
+        result['Customer Status'] = 'Thank you for being valued customer'
+        return jsonify(result)
+
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=8888)
 
 
